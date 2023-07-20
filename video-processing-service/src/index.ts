b4fb27b8c7363f5eb32c290a94a96ade489aeabe
masa73044 <108378047+masa73044@users.noreply.github.com>
2023-07-20 09:01:10 +0700

    storage

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import ffmpeg from "fluent-ffmpeg";
+import { setupDirectories } from "./storage";
+
+setupDirectories();
 
 const app = express();
 app.use(express.json());
@@ -17,17 +20,6 @@ app.post("/process-video", (req, res) => {
   } else if (!outputFilePath) {
     res.status(400).send("Bad Request: Missing output file path");
   }
-
-  ffmpeg(inputFilePath)
-    .outputOptions("-vf", "scale=-1:360")
-    .on("end", () => {
-      res.status(200).send("Video process finished successfully.");
-    })
-    .on("error", (err) => {
-      console.log(`An error occured: ${err.message}`);
-      res.status(500).send(`Internal Server Error: ${err.message}`);
-    })
-    .save(outputFilePath);
 });
 
 const port = process.env.PORT || 8080;
--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -1,6 +1,8 @@
 import { Storage } from "@google-cloud/storage";
 import fs from "fs";
 import ffmpeg from "fluent-ffmpeg";
+import { resolve } from "path";
+import { dir } from "console";
 
 // GCS & local file interactions
 
@@ -14,9 +16,82 @@ const processedVideoBucketName = "mk-yt-processed-videos";
 const localRawVideoPath = "./raw-videos";
 const localProcessedVideoPath = "./processed-videos";
 
-export function setupDirectories() {}
+export function setupDirectories() {
+  ensureDirectoryExistence(localRawVideoPath);
+  ensureDirectoryExistence(localProcessedVideoPath);
+}
 
-export function convertVideo(
-  rawVideoName: string,
-  processedVideoName: string
-) {}
+export function convertVideo(rawVideoName: string, processedVideoName: string) {
+  return new Promise<void>((resolve, reject) => {
+    ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
+      .outputOptions("-vf", "scale=-1:360")
+      .on("end", () => {
+        console.log("Video process finished successfully.");
+        resolve();
+      })
+      .on("error", (err) => {
+        console.log(`An error occured: ${err.message}`);
+        reject(err);
+      })
+      .save(`${localProcessedVideoPath}/${processedVideoName}`);
+  });
+}
+
+export async function downloadRawVideo(fileName: string) {
+  await storage
+    .bucket(rawVideoBucketName)
+    .file(fileName)
+    .download({ destination: `${localRawVideoPath}/${fileName}` });
+
+  console.log(
+    `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}`
+  );
+}
+
+export async function uploadProcessedVideo(fileName: string) {
+  const bucket = storage.bucket(processedVideoBucketName);
+
+  await bucket.upload(`${localProcessedVideoPath}/${fileName}`, {
+    destination: fileName,
+  });
+
+  console.log(
+    `${localProcessedVideoPath}/${fileName} uploaded to ${processedVideoBucketName}/${fileName}`
+  );
+
+  await bucket.file(fileName).makePublic();
+}
+
+export function deleteRawVideo(fileName: string) {
+  return deletefile(`${localRawVideoPath}/${fileName}`);
+}
+
+export function deleteProcessedVideo(fileName: string) {
+  return deletefile(`${localProcessedVideoPath}/${fileName}`);
+}
+
+function deletefile(filePath: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(`Failed to delete file at ${filePath} `, err);
+          reject(err);
+        } else {
+          console.log(`File deleted at ${filePath}`);
+          resolve();
+        }
+      });
+    } else {
+      console.log(`File not found at ${filePath}, skipping the delete`);
+      resolve();
+    }
+  });
+}
+
+function ensureDirectoryExistence(dirPath: string) {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+    console.log(`Directory created at ${dirPath}`);
+  }
+}
